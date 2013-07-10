474294e0dbd2e34ed2b120a9b6eca54a1d2ea3d4
lang <bm2736892@gmail.com>
2013-07-10 09:25:30 +0800

    Improve svg loader

--- src/loader/svg.js
+++ src/loader/svg.js
@@ -13,8 +13,11 @@ define(function(require) {
     var Rectangle = require("2d/shape/rectangle");
     var Ellipse = require("2d/shape/ellipse");
     var Line = require("2d/shape/line");
+    var Path = require("2d/shape/path");
+    var Polygon = require("2d/shape/polygon");
     var TextBox = require("2d/shape/textbox");
-    var SVGPath = require("2d/shape/svgpath"); 
+    var SVGPath = require("2d/shape/svgpath");
+    var _ = require("_");
 
     var Style = require("2d/style");
 
@@ -42,27 +45,29 @@ define(function(require) {
                 }
             })
         },
-        parse : function(xmlString, callback) {
-            var parser = new DOMParser();
-            var doc = parser.parseFromString(xmlString, 'text/xml');
-            var svg = doc.firstChild;
-
+        parse : function(xml, callback) {
+            if (typeof(xml) === "string") {
+                var parser = new DOMParser();
+                var doc = parser.parseFromString(xml, 'text/xml');
+                var svg = doc.firstChild;
+            } else {
+                var svg = xml;
+            }
             var root = new Node();
             
             // parse view port
-            var viewBox = svg.getAttribute("viewBox");
+            var viewBox = svg.getAttribute("viewBox") || '';
             var viewBoxArr = viewBox.split(/\s+/);
 
-            var x = parseFloat(viewBoxArr[0]);
-            var y = parseFloat(viewBoxArr[1]);
+            var width = parseFloat(svg.getAttribute("width") || 0);
+            var height = parseFloat(svg.getAttribute("height") || 0);
+
+            var x = parseFloat(viewBoxArr[0] || 0);
+            var y = parseFloat(viewBoxArr[1] || 0);
             var vWidth = parseFloat(viewBoxArr[2]);
             var vHeight = parseFloat(viewBoxArr[3]);
 
-            var width = parseFloat(svg.getAttribute("width") || vWidth);
-            var height = parseFloat(svg.getAttribute("height") || vHeight);
-
             root.position.set(x, y);
-            root.scale.set(width / vWidth, height / vHeight);
 
             var child = svg.firstChild;
             while (child) {
@@ -76,7 +81,8 @@ define(function(require) {
         },
 
         _parseNode : function(xmlNode, parent) {
-            var nodeParser = nodeParsers[xmlNode.nodeName];
+            var nodeName = xmlNode.nodeName.toLowerCase();
+            var nodeParser = nodeParsers[nodeName];
             if (nodeParser) {
                 var node = nodeParser(xmlNode, parent);
                 parent.add(node);
@@ -102,10 +108,10 @@ define(function(require) {
             _inheritStyle(parentNode, rect);
             _parseAttributes(xmlNode, rect);
 
-            var x = parseInt(xmlNode.getAttribute("x") || 0);
-            var y = parseInt(xmlNode.getAttribute("y") || 0);
-            var width = parseInt(xmlNode.getAttribute("width") || 0);
-            var height = parseInt(xmlNode.getAttribute("height") || 0);
+            var x = parseFloat(xmlNode.getAttribute("x") || 0);
+            var y = parseFloat(xmlNode.getAttribute("y") || 0);
+            var width = parseFloat(xmlNode.getAttribute("width") || 0);
+            var height = parseFloat(xmlNode.getAttribute("height") || 0);
             rect.start.set(x, y);
             rect.size.set(x, y);
 
@@ -116,18 +122,82 @@ define(function(require) {
             _inheritStyle(parentNode, circle);
             _parseAttributes(xmlNode, circle);
 
-            var cx = parseInt(xmlNode.getAttribute("cx") || 0);
-            var cy = parseInt(xmlNode.getAttribute("cy") || 0);
-            var r = parseInt(xmlNode.getAttribute("r") || 0);
+            var cx = parseFloat(xmlNode.getAttribute("cx") || 0);
+            var cy = parseFloat(xmlNode.getAttribute("cy") || 0);
+            var r = parseFloat(xmlNode.getAttribute("r") || 0);
             circle.center.set(cx, cy);
             circle.radius = r;
 
             return circle;
         },
+        'line' : function(xmlNode, parentNode){
+            var line = new Line();
+            _inheritStyle(parentNode, line);
+            _parseAttributes(xmlNode, line);
+
+            var x1 = parseFloat(xmlNode.getAttribute("x1") || 0);
+            var y1 = parseFloat(xmlNode.getAttribute("y1") || 0);
+            var x2 = parseFloat(xmlNode.getAttribute("x2") || 0);
+            var y2 = parseFloat(xmlNode.getAttribute("y2") || 0);
+            line.start.set(x1, y1);
+            line.end.set(x2, y2);
+
+            return line;
+        },
+        "ellipse" : function(xmlNode, parentNode) {
+            var ellipse = new Ellipse();
+            _inheritStyle(parentNode, ellipse);
+            _parseAttributes(xmlNode, ellipse);
+
+            var cx = parseFloat(xmlNode.getAttribute("cx") || 0);
+            var cy = parseFloat(xmlNode.getAttribute("cy") || 0);
+            var rx = parseFloat(xmlNode.getAttribute("rx") || 0);
+            var ry = parseFloat(xmlNode.getAttribute("ry") || 0);
+
+            ellipse.center.set(cx, cy);
+            ellipse.radius.set(rx, ry);
+            return ellipse;
+        },
+        'polygon' : function(xmlNode, parentNode) {
+            var points = xmlNode.getAttribute("points");
+            if (points) {
+                points = _parsePoints(points);
+            }
+            var polygon = new Polygon({
+                points : points
+            });
+            _inheritStyle(parentNode, polygon);
+            _parseAttributes(xmlNode, polygon);
+
+            return polygon;
+        },
+        'polyline' : function(xmlNode, parentNode) {
+            var path = new Path();
+            _inheritStyle(parentNode, path);
+            _parseAttributes(xmlNode, path);
+
+            var points = xmlNode.getAttribute("points");
+            if (points) {
+                points = _parsePoints(points);
+                path.pushPoints(points);
+            }
+
+            return path;
+        },
+        'image' : function(xmlNode, parentNode) {
+
+        },
+        'text' : function(xmlNode, parentNode) {
+            
+        },
         "path" : function(xmlNode, parentNode) {
             var path = new SVGPath();
             _inheritStyle(parentNode, path);
             _parseAttributes(xmlNode, path);
+
+            // TODO svg fill rule
+            // https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/fill-rule
+            // path.style.globalCompositeOperation = 'xor';
 
             var d = xmlNode.getAttribute("d") || "";
             path.description = d;
@@ -136,92 +206,136 @@ define(function(require) {
         }
     }
 
-    function _inheritStyle(parent, child){
+    function _inheritStyle(parent, child) {
         child.stroke = parent.stroke;
         child.fill = parent.fill;
     }
 
+    function _parsePoints(pointsString) {
+        var list = pointsString.trim().replace(/,/g, " ").split(/\s+/);
+        var points = [];
+
+        for (var i = 0; i < list.length; i+=2) {
+            var x = parseFloat(list[i]);
+            var y = parseFloat(list[i+1]);
+            points.push(new Vector2(x, y));
+        }
+        return points;
+    }
+
     function _parseAttributes(xmlNode, node) {
         _parseTransformAttribute(xmlNode, node);
-        _parseStyleAttribute(xmlNode, node);
 
-        var fill = xmlNode.getAttribute("fill");
-        var stroke = xmlNode.getAttribute("stroke");
-        var strokeWidth = xmlNode.getAttribute("stroke-width");
-        var opacity = xmlNode.getAttribute("opacity");
-
-        if (fill) {
-            node.style.fillStyle = _getPaint(fill);
+        var styleList = {
+            fill : xmlNode.getAttribute('fill'),
+            stroke : xmlNode.getAttribute("stroke"),
+            lineWidth : xmlNode.getAttribute("stroke-width"),
+            opacity : xmlNode.getAttribute('opacity'),
+            lineDash : xmlNode.getAttribute('stroke-dasharray'),
+            lineDashOffset : xmlNode.getAttribute('stroke-dashoffset'),
+            lineCap : xmlNode.getAttribute('stroke-linecap'),
+            lineJoin : xmlNode.getAttribute('stroke-linjoin'),
+            miterLimit : xmlNode.getAttribute("stroke-miterlimit")
         }
-        if (stroke) {
+
+        _.extend(styleList, _parseStyleAttribute(xmlNode));
+
+        node.style = new Style({
+            fill : _getPaint(styleList.fill),
+            stroke : _getPaint(styleList.stroke),
+            lineWidth : parseFloat(styleList.lineWidth),
+            opacity : parseFloat(styleList.opacity),
+            lineDashOffset : styleList.lineDashOffset,
+            lineCap : styleList.lineCap,
+            lineJoin : styleList.lineJoin,
+            miterLimit : parseFloat(styleList.miterLimit)
+        });
+        if (styleList.lineDash) {
+            node.style.lineDash = styleList.lineDash.trim().split(/\s*,\s*/);
+        }
+
+        if (styleList.stroke && styleList.stroke !== "none") {
+            // enable stroke
             node.stroke = true;
-            node.style.strokeStyle = _getPaint(stroke);
-        }
-        if (strokeWidth) {
-            node.style.lineWidth = parseFloat(strokeWidth);
-        }
-        if (opacity) {
-            node.style.globalAlpha = parseFloat(opacity);
         }
     }
+
 
     function _getPaint(str) {
         return str;
     }
 
-    var transformRegex = /(translate|scale|rotate|skewX|skewY|matrix)\(([\s0-9\.,]*)\)/
+    var transformRegex = /(translate|scale|rotate|skewX|skewY|matrix)\(([\-\s0-9\.,]*)\)/g;
+
     function _parseTransformAttribute(xmlNode, node) {
         var transform = xmlNode.getAttribute("transform");
         if (transform) {
-            var transformList = transform.split(/\s+/);
-
-            for(var i = 0; i < transformList.length; i++) {
-                var item = transformList[i];
-
-                var result = transformRegex.exec(item);
-                if (result) {
-                    var type = result[1];
-                    var m = node.transform;
-                    switch(type) {
-                        case "translate":
-                            var value = result[2].trim().split(/\s*/);
-                            m.translate(new Vector2(parseFloat(value[0]), parseFloat(value[1] || 0)));
-                            break;
-                        case "scale":
-                            var value = result[2].trim().split(/\s*/);
-                            m.scale(new Vector2(parseFloat(value[0]), parseFloat(value[1])));
-                            break;
-                        case "rotate":
-                            m.rotate(parseFloat(result[2]));
-                            break;
-                        case "skew":
-                            var value = result[2].trim().split(/\s*/);
-                            console.warn("Skew transform is not supported yet");
-                            break;
-                        case "matrix":
-                            var value = result[2].trim().split(/\s*,\s*/);
-                            var arr = m._array;
-                            arr[0] = parseFloat(value[0]);
-                            arr[1] = parseFloat(value[1]);
-                            arr[2] = parseFloat(value[2]);
-                            arr[3] = parseFloat(value[3]);
-                            arr[4] = parseFloat(value[4]);
-                            arr[5] = parseFloat(value[5]);
-                            break;
-                    }
+            var m = node.transform;
+            m.identity();
+            var transformOps = [];
+            transform.replace(transformRegex, function(str, type, value){
+                transformOps.push(type, value);
+            })
+            for(var i = transformOps.length-1; i > 0; i-=2){
+                var value = transformOps[i];
+                var type = transformOps[i-1];
+                switch(type) {
+                    case "translate":
+                        value = value.trim().split(/\s+/);
+                        m.translate(new Vector2(parseFloat(value[0]), parseFloat(value[1] || 0)));
+                        break;
+                    case "scale":
+                        value = value.trim().split(/\s+/);
+                        m.scale(new Vector2(parseFloat(value[0]), parseFloat(value[1] || value[0])));
+                        break;
+                    case "rotate":
+                        value = value.trim().split(/\s*/);
+                        m.rotate(parseFloat(value[0]));
+                        break;
+                    case "skew":
+                        value = value.trim().split(/\s*/);
+                        console.warn("Skew transform is not supported yet");
+                        break;
+                    case "matrix":
+                        var value = value.trim().split(/\s*,\s*/);
+                        var arr = m._array;
+                        arr[0] = parseFloat(value[0]);
+                        arr[1] = parseFloat(value[1]);
+                        arr[2] = parseFloat(value[2]);
+                        arr[3] = parseFloat(value[3]);
+                        arr[4] = parseFloat(value[4]);
+                        arr[5] = parseFloat(value[5]);
+                        break;
                 }
             }
         }
         node.autoUpdate = false;
     }
 
-    function _parseStyleAttribute(xmlNode, node) {
+    var styleRegex = /(\S*?):(.*?);/g;
+    function _parseStyleAttribute(xmlNode) {
         var style = xmlNode.getAttribute("style");
-        node.style = new Style();
-        if (style) {
 
+        if (style) {
+            var styleList = {};
+            style = style.replace(/\s*([;:])\s*/g, "$1");
+            style.replace(styleRegex, function(str, key, val){
+                styleList[key] = val;
+            });
+
+            return {
+                fill : styleList['fill'],
+                stroke : styleList['stroke'],
+                lineWidth : styleList['stroke-width'],
+                opacity : styleList['opacity'],
+                lineDash : styleList['stroke-dasharray'],
+                lineDashOffset : styleList['stroke-dashoffset'],
+                lineCap : styleList['stroke-linecap'],
+                lineJoin : styleList['stroke-linjoin'],
+                miterLimit : styleList['stroke-miterlimit']
+            }
         }
-        return node;
+        return {};
     }
 
     function _parseCSSRules(doc) {
