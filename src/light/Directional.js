4a7f39cda1822f1abe0171c08c6ee7aef8a31633
lang <bm2736892@gmail.com>
2016-12-21 10:40:59 +0800

    Adjust directional shadow bias

--- src/light/Directional.js
+++ src/light/Directional.js
@@ -24,7 +24,7 @@ define(function (require) {
         /**
          * @type {number}
          */
-        shadowBias: 0.0002,
+        shadowBias: 0.001,
         /**
          * @type {number}
          */
