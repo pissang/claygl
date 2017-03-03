69f34e1ff649014f0ad1aab9a7eabd065044282d
lang <bm2736892@gmail.com>
2017-03-03 15:17:12 +0800

    Tweak

--- src/picking/RayPicking.js
+++ src/picking/RayPicking.js
@@ -114,23 +114,14 @@ define(function(require) {
 
                 var point;
                 var indices = geometry.indices;
-                var positions = geometry.attributes.position.value;
+                var positionsAttr = geometry.attributes.position;
                 for (var i = 0; i < indices.length;) {
-                    var i1 = indices[i++] * 3;
-                    var i2 = indices[i++] * 3;
-                    var i3 = indices[i++] * 3;
-
-                    v1._array[0] = positions[i1];
-                    v1._array[1] = positions[i1 + 1];
-                    v1._array[2] = positions[i1 + 2];
-
-                    v2._array[0] = positions[i2];
-                    v2._array[1] = positions[i2 + 1];
-                    v2._array[2] = positions[i2 + 2];
-
-                    v3._array[0] = positions[i3];
-                    v3._array[1] = positions[i3 + 1];
-                    v3._array[2] = positions[i3 + 2];
+                    var i1 = indices[i++];
+                    var i2 = indices[i++];
+                    var i3 = indices[i++];
+                    positionsAttr.get(i1, v1._array);
+                    positionsAttr.get(i2, v2._array);
+                    positionsAttr.get(i3, v3._array);
 
                     if (cullBack) {
                         point = ray.intersectTriangle(v1, v2, v3, renderable.culling);
@@ -142,7 +133,7 @@ define(function(require) {
                         var pointW = new Vector3();
                         Vector3.transformMat4(pointW, point, renderable.worldTransform);
                         out.push(new RayPicking.Intersection(
-                            point, pointW, renderable, [i1 / 3, i2 / 3, i3 / 3],
+                            point, pointW, renderable, [i1, i2, i3],
                             Vector3.dist(pointW, this._ray.origin)
                         ));
                     }
