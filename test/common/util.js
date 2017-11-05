5fb4d7d8a25739e530988b380df6161799894822
fuzhenn <fuzhen@maptalks.org>
2017-11-05 23:34:35 +0800

    fix assertWith by using pixel comparison

--- test/common/util.js
+++ test/common/util.js
@@ -2,6 +2,10 @@ const fs = require('fs');
 const nativeImage = require('electron').nativeImage;
 const assert = require('assert');
 
+const canvas1 = document.createElement('canvas');
+const canvas2 = document.createElement('canvas');
+const glImg = document.createElement('canvas');
+
 module.exports = {
     /**
     * Get image content of the canvas
@@ -20,12 +24,11 @@ module.exports = {
 
         const clamped = new Uint8ClampedArray(pixels),
             imageData = new ImageData(clamped, width, height);
-        const img = document.createElement('canvas');
-        img.width = width;
-        img.height = height;
-        const ctx = img.getContext('2d');
+        glImg.width = width;
+        glImg.height = height;
+        const ctx = glImg.getContext('2d');
         ctx.putImageData(imageData, 0, 0);
-        const data = img.toDataURL('image/png');
+        const data = glImg.toDataURL('image/png');
         const buf = nativeImage.createFromDataURL(data).toPng();
         return buf;
     }, 
@@ -49,8 +52,45 @@ module.exports = {
         } else {
             const expected = fs.readFileSync(filePath);
             const actual = this.getGlImage(canvas);
-            assert.deepEqual(expected, actual, filePath);
+            assert(this.compareImagePixel(expected, actual), filePath);
             cb();
         }
+    },
+
+    compareImagePixel(buf1, buf2) {
+        const img1 = nativeImage.createFromBuffer(buf1);
+        const img2 = nativeImage.createFromBuffer(buf2);
+        const size1 = img1.getSize(),
+            size2 = img2.getSize();
+        if (size1.width !== size2.width || size1.height !== size2.height) {
+            return false;
+        }
+
+        const w = size1.width, h = size1.height;
+
+        const webImg1 = new Image();
+        webImg1.src = img1.toDataURL();
+
+        const webImg2 = new Image();
+        webImg2.src = img2.toDataURL();
+
+        canvas1.width = canvas2.width = w;
+        canvas1.height = canvas2.height = h;
+        const ctx1 = canvas1.getContext('2d');
+        const ctx2 = canvas2.getContext('2d');
+
+        ctx1.drawImage(webImg1, 0, 0);
+        ctx2.drawImage(webImg2, 0, 0);
+
+        const data1 = ctx1.getImageData(0, 0, w, h).data;
+        const data2 = ctx2.getImageData(0, 0, w, h).data;
+        
+        for (let i = 0, l = data1.length; i < l; i += 4) {
+            if (data1[i] !== data2[i] || data1[i + 1] !== data2[i + 1] || data1[i + 2] !== data2[i + 2] || data1[i + 3] !== data2[i + 3]) {
+                return false;
+            }
+        }
+        return true;
     }
+
 };
--- test/istanbul-reporter.js
+++ test/istanbul-reporter.js
@@ -1,16 +1,20 @@
 const istanbulAPI = require('istanbul-api');
 const libCoverage = require('istanbul-lib-coverage');
+const Spec = require('mocha/lib/reporters/spec');
 
-function Istanbul(runner) {
-  runner.on('end', () => {
-    const mainReporter = istanbulAPI.createReporter();
-    const coverageMap = libCoverage.createCoverageMap();
+class Istanbul extends Spec {
+  constructor(runner) {
+    super(runner);
+    runner.on('end', () => {
+      const mainReporter = istanbulAPI.createReporter();
+      const coverageMap = libCoverage.createCoverageMap();
 
-    coverageMap.merge(global.__coverage__ || {});
+      coverageMap.merge(global.__coverage__ || {});
 
-    mainReporter.addAll(['lcovonly', 'json']);
-    mainReporter.write(coverageMap, {});
-  });
+      mainReporter.addAll(['lcovonly', 'json']);
+      mainReporter.write(coverageMap, {});
+    });
+  }
 }
 
 
