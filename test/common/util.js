6f49ed465446c5844f977adc0adb2132c49ffbd6
fuzhenn <fuzhen@maptalks.org>
2017-11-06 17:27:20 +0800

    add blank assertion and add shadow specs

--- test/common/util.js
+++ test/common/util.js
@@ -50,47 +50,60 @@ module.exports = {
                 cb();        
             });
         } else {
-            const expected = fs.readFileSync(filePath);
+            const fixture = fs.readFileSync(filePath);
             const actual = this.getGlImage(canvas);
-            assert(this.compareImagePixel(expected, actual), filePath);
-            cb();
+            this._loadImage(fixture, actual, (fixture, actual) => {
+                const ret = this.compareImagePixel(fixture, actual);
+                assert(ret === 1, (ret === -1 ? 'Image is different from fixture' : 'Image is blank') + `: ${filePath}`);
+                cb();
+            });
         }
     },
 
-    compareImagePixel(buf1, buf2) {
-        const img1 = nativeImage.createFromBuffer(buf1);
-        const img2 = nativeImage.createFromBuffer(buf2);
-        const size1 = img1.getSize(),
-            size2 = img2.getSize();
-        if (size1.width !== size2.width || size1.height !== size2.height) {
-            return false;
-        }
-
-        const w = size1.width, h = size1.height;
+    _loadImage(fixture, actual, cb) {
+        const img1 = nativeImage.createFromBuffer(fixture);
+        const img2 = nativeImage.createFromBuffer(actual);
 
         const webImg1 = new Image();
-        webImg1.src = img1.toDataURL();
-
         const webImg2 = new Image();
+        webImg1.onload = onload;
+        webImg2.onload = onload;
+        function onload() {
+            this.finished = true;
+            if (webImg1.finished && webImg2.finished) {
+                cb(webImg1, webImg2);
+            }
+        }
+        webImg1.src = img1.toDataURL();
         webImg2.src = img2.toDataURL();
+    },
 
+    compareImagePixel(fixture, actual) {
+        if (fixture.width !== actual.width || fixture.height !== actual.height) {
+            return -1;
+        }
+        const w = fixture.width, h = fixture.height;
         canvas1.width = canvas2.width = w;
         canvas1.height = canvas2.height = h;
         const ctx1 = canvas1.getContext('2d');
         const ctx2 = canvas2.getContext('2d');
 
-        ctx1.drawImage(webImg1, 0, 0);
-        ctx2.drawImage(webImg2, 0, 0);
+        ctx1.drawImage(fixture, 0, 0);
+        ctx2.drawImage(actual, 0, 0);
 
         const data1 = ctx1.getImageData(0, 0, w, h).data;
         const data2 = ctx2.getImageData(0, 0, w, h).data;
         
+        let drawn = 0;
         for (let i = 0, l = data1.length; i < l; i += 4) {
+            if (!drawn && data2[i + 3] > 0) {
+                drawn = 1;
+            }
             if (data1[i] !== data2[i] || data1[i + 1] !== data2[i + 1] || data1[i + 2] !== data2[i + 2] || data1[i + 3] !== data2[i + 3]) {
-                return false;
+                return -1;
             }
         }
-        return true;
+        return drawn;
     }
 
 };
