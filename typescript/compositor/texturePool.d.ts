d44f66fdef2fd4ecfa26f30360e491e7de15ccbd
pissang <bm2736892@gmail.com>
2018-01-03 16:12:30 +0800

    Remove typescript interfaces.

--- typescript/compositor/texturePool.d.ts
+++
@@ -1,9 +0,0 @@
-///<reference path="../Texture.d.ts" />
-declare module qtek.compositor.texturePool {
-
-    export function get(parameters: ITextureOption): Texture;
-
-    export function put(parameters: ITextureOption): void;
-
-    export function clear(gl: WebGLRenderingContext): void;
-}
--- typescript/util/mesh.d.ts
+++
@@ -1,9 +0,0 @@
-///<reference path="../Mesh.d.ts" />
-///<reference path="../Node.d.ts" />
-declare module qtek.util.mesh {
-
-    export function merge(meshList: Mesh[], applyWorldTransform?: boolean): Mesh;
-
-    export function splitByJoints(mesh: Mesh, maxJointNumber: number, inPlace: boolean): Node;
-
-}
