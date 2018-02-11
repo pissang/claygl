9ec6cb8002506063fe83a9998a4d5bf4b6f6ddc6
pissang <bm2736892@gmail.com>
2018-02-11 18:39:18 +0800

    Fix disposeNode disposeTexture parameter

--- build/glsl2js.js
+++ build/glsl2js.js
@@ -6,13 +6,17 @@ var ROOT = __dirname + '/../src/';
 glob(ROOT + '**/*.glsl', function (err, files) {
     files.forEach(function (filePath) {
         var glslCode = fs.readFileSync(filePath, 'utf-8');
-        
+
+        // var temporalPlaceHolder = '__clay_meta_start_temporal_placeholder_' + Math.round(Math.random() * 10000);
         //From THREE.js's rollup.config.js
         glslCode = glslCode
-            .replace( /\r/g, '' ) // remove \r
-            .replace( /[ \t]*\/\/.*\n/g, '' ) // remove //
-            .replace( /[ \t]*\/\*[\s\S]*?\*\//g, '' ) // remove /* */
-            .replace( /\n{2,}/g, '\n' ) // # \n+ to \n
+            .replace(/\r/g, '' ) // remove \r
+            // Not replace ///
+            // .replace(/\/\/\//g, temporalPlaceHolder)
+            .replace(/[ \t]*\/\/.*\n/g, '' ) // remove //
+            .replace(/[ \t]*\/\*[\s\S]*?\*\//g, '' ) // remove /* */
+            .replace(/\n{2,}/g, '\n' ); // # \n+ to \n
+            // .replace(new RegExp(temporalPlaceHolder, 'g'), '///');
 
         // var dir = path.dirname(filePath);
         // var baseName = path.basename(filePath, '.essl');
--- src/Renderer.js
+++ src/Renderer.js
@@ -859,9 +859,33 @@ var Renderer = Base.extend(function () {
         if (root.getParent()) {
             root.getParent().remove(root);
         }
+        var disposedMap = {};
         root.traverse(function(node) {
+            var material = node.material;
             if (node.geometry && disposeGeometry) {
                 node.geometry.dispose(this);
+            }
+            if (disposeTexture && material && !disposedMap[material.__uid__]) {
+                var textureUniforms = material.getTextureUniforms();
+                for (var u = 0; u < textureUniforms.length; u++) {
+                    var uniformName = textureUniforms[u];
+                    var val = material.uniforms[uniformName].value;
+                    var uniformType = material.uniforms[uniformName].type;
+                    if (!val) {
+                        continue;
+                    }
+                    if (uniformType === 't') {
+                        val.dispose && val.dispose(this);
+                    }
+                    else if (uniformType === 'tv') {
+                        for (var k = 0; k < val.length; k++) {
+                            if (val[k]) {
+                                val[k].dispose && val[k].dispose(this);
+                            }
+                        }
+                    }
+                }
+                disposedMap[material.__uid__] = true;
             }
             // Particle system and AmbientCubemap light need to dispose
             if (node.dispose) {
