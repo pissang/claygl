cbdf3c86f341b124384e6c74a4b34f4390a43eb6
Derek Wang <birdinforest@gmail.com>
2020-10-10 14:00:04 +1100

    Should be right to `Math.max` rather than `Math.ceil`.

--- src/Texture2D.js
+++ src/Texture2D.js
@@ -117,8 +117,8 @@ var Texture2D = Texture.extend(function () {
             for (var i = 0; i < this.mipmaps.length; i++) {
                 var mipmap = this.mipmaps[i];
                 this._updateTextureData(_gl, mipmap, i, width, height, glFormat, glType, false);
-                width = Math.ceil(width / 2);
-                height = Math.ceil(height / 2);
+                width = Math.max(width / 2);
+                height = Math.max(height / 2);
             }
         }
         else {
