38f1ad0cc05987d6acc95c1e456ec15e391a9c6d
pissang <bm2736892@gmail.com>
2023-02-09 15:50:06 +0800

    fix vite config

--- example/vite.config.js
+++ example/vite.config.js
@@ -1,14 +1,25 @@
 import { defineConfig } from 'vite';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+var filename = fileURLToPath(import.meta.url);
+var dirname = path.dirname(filename);
 
 export default defineConfig({
   server: {
     hmr: false
   },
+  build: {
+    rollupOptions: {
+      input: {
+        triangle_customshader: path.resolve(dirname, './triangle_customshader.html')
+      }
+    }
+  },
   resolve: {
     alias: {
-      'claygl/shaders': path.resolve(__dirname, '../src/shader/exports.ts'),
-      claygl: path.resolve(__dirname, '../src/claygl.ts')
+      'claygl/shaders': path.resolve(dirname, '../src/shader/exports.ts'),
+      claygl: path.resolve(dirname, '../src/claygl.ts')
     }
   }
 });
