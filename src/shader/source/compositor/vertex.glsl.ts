ffac874c160b72741c975c8c48460f633846c74f
pissang <bm2736892@gmail.com>
2023-03-14 15:49:31 +0800

    optimize fullquad shader

--- src/shader/source/compositor/vertex.glsl.ts
+++ src/shader/source/compositor/vertex.glsl.ts
@@ -1,27 +1,17 @@
-import {
-  createAttribute,
-  createSemanticUniform,
-  createVarying,
-  glsl,
-  VertexShader
-} from '../../../Shader';
+import { createAttribute, createVarying, glsl, VertexShader } from '../../../Shader';
 
 export const fullscreenQuadPassVertex = new VertexShader({
   name: 'fullQuadVertex',
-  uniforms: {
-    WVP: createSemanticUniform('mat4', 'WORLDVIEWPROJECTION')
-  },
   attributes: {
-    pos: createAttribute('vec3', 'POSITION'),
-    uv: createAttribute('vec2', 'TEXCOORD_0')
+    pos: createAttribute('vec3', 'POSITION')
   },
   varyings: {
     v_Texcoord: createVarying('vec2')
   },
   main: glsl`
 void main() {
-  v_Texcoord = uv;
-  gl_Position = WVP * vec4(pos, 1.0);
+  v_Texcoord = pos.xy * 0.5 + 0.5;
+  gl_Position = vec4(pos, 1.0);
 }
   `
 });
